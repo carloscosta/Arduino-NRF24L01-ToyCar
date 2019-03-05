f290342229df9cbdd2e331bdd79b84787e927103
Carlos Costa <crncosta@gmail.com>
2019-03-05 16:40:17 -0300

    Moved to use RF 433 MHZ and the excelent RadioHead library

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,23 +1,201 @@
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+
+// Include dependant SPI Library
 #include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+#define DEBUG 1
+#define BUZ 13
 
-const byte address[6] = "00001";
+// motor one
+#define ENA 10
+#define IN1 3
+#define IN2 4
 
-void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+// motor two
+#define ENB 9
+#define IN3 5
+#define IN4 6
+
+#define HURRY 250
+#define SLUE 200
+
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+
+/* 0: left;
+   1: right;
+   2: honk;
+   3: front;
+   4: back;
+   5: panic */
+uint8_t buf[6] = {0, 0, 0, 0, 0, 0};
+
+void startup_song()
+{
+  tone(BUZ, 262, 200); //DO
+  delay(200);
+  tone(BUZ, 294, 300); //RE
+  delay(200);
+  tone(BUZ, 330, 300); //MI
+  delay(200);
+  tone(BUZ, 349, 300); //FA
+  delay(300);
+  tone(BUZ, 349, 300); //FA
+  delay(300);
+  tone(BUZ, 349, 300); //FA
+  delay(300);
 }
 
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+void bibi()
+{
+  tone (BUZ, 330, 300); // MI
+  delay(500); // tempo
+  tone (BUZ, 330, 300); // MI
+
+  delay(2000); // tempo
+}
+
+void turn_off()
+{
+#ifdef DEBUG
+  Serial.println("turn off all motors");
+#endif
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void left()
+{
+#ifdef DEBUG
+  Serial.println("turn left motor direction");
+#endif
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, SLUE);
+}
+
+void right()
+{
+#ifdef DEBUG
+  Serial.println("turn motor direction to right");
+#endif
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, SLUE);
+}
+
+void center()
+{
+#ifdef DEBUG
+  Serial.println("keep direction centered");
+#endif
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
+}
+
+void forward()
+{
+#ifdef DEBUG
+  Serial.println("forward");
+#endif
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, HURRY);
+}
+
+void backward()
+{
+#ifdef DEBUG
+  Serial.println("backward");
+#endif
+  // turn motor direction
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // accelerate from zero to maximum speed
+  for (int i = 0; i < HURRY; i++)
+  {
+    analogWrite(ENA, i);
+    delay(10);
   }
+}
+
+void setup()
+{
+  pinMode(BUZ, OUTPUT); // buzzer
+
+  // set all the motor control pins to outputs
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+
+#ifdef DEBUG
+  Serial.begin(9600);
+  Serial.println("----- Start Serial Monitor -----");
+#endif
+
+  // Initialize ASK Object
+  rf_driver.init();
+
+  startup_song();
+}
+
+void loop()
+{
+  uint8_t buflen = sizeof(buf);
+
+  // Check if received packet is correct size
+  if (rf_driver.recv(buf, &buflen))
+  {
+
+#ifdef DEBUG
+    // Message received with valid checksum
+    Serial.print("Message Received: ");
+    for (int i = 0; i < 6; i++)
+      Serial.print(buf[i]);
+    Serial.println(" ");
+#endif
+
+    // honk if requested
+    if (buf[2] > 0) bibi();
+
+    // evaluate left/right early
+    if (buf[0] > 0 && buf[1] == 0)
+    {
+      left();
+    }
+    else if (buf[0] == 0 && buf[1] > 0)
+    {
+      right();
+    }
+    else
+    {
+      center();
+    }
+
+    // then evaluate forward/backward actions
+    if (buf[3] > 0 && buf[4] == 0)
+    {
+      forward();
+    }
+    else if (buf[3] == 0 && buf[4] > 0)
+    {
+      backward();
+    }
+    else
+    {
+      turn_off();
+    }
+  }
+  delay(200);
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,20 +1,145 @@
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+
+// Include dependant SPI Library
 #include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+#define DEBUG 1
+#define BUZ 9
 
-const byte address[6] = "00001";
+#define LVRx A0
+#define LVRy A1
+#define Lsw  6
 
-void setup() {
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+#define RVRx A2
+#define RVRy A3
+#define Rsw  7
+
+int val_lvrx = 0;
+
+int val_rvrx = 0;
+int val_rvry = 0;
+
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+
+/* 0: left;
+   1: right;
+   2: honk;
+   3: front;
+   4: back;
+   5: panic */
+uint8_t buf[6] = {0, 0, 0, 0, 0, 0};
+
+void startup_song()
+{
+  tone(BUZ, 262, 200); //DO
+  delay(200);
+  tone(BUZ, 294, 300); //RE
+  delay(200);
+  tone(BUZ, 330, 300); //MI
+  delay(200);
+  tone(BUZ, 349, 300); //FA
+  delay(300);
+  tone(BUZ, 349, 300); //FA
+  delay(300);
+  tone(BUZ, 349, 300); //FA
+  delay(300);
 }
 
-void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+void bibi()
+{
+  tone (BUZ, 330, 300); // MI
+  delay(500); // tempo
+  tone (BUZ, 330, 300); // MI
+
+  delay(2000); // tempo
+}
+
+void setup()
+{
+  pinMode(BUZ, OUTPUT); // buzzer
+
+#ifdef DEBUG
+  Serial.begin(9600);
+  Serial.println("----- Start Serial Monitor -----");
+#endif
+
+  // Initialize ASK Object
+  rf_driver.init();
+
+  startup_song();
+}
+
+void loop()
+{
+  val_lvrx = analogRead(LVRx);
+  val_lvrx = map(val_lvrx, 0, 1023, 0, 255);
+  val_lvrx = constrain(val_lvrx, 0, 255);
+
+#ifdef DEBUG
+  Serial.print("LVRx = ");
+  Serial.println(val_lvrx);
+#endif
+
+  val_rvry = analogRead(RVRy);
+  val_rvry = map(val_rvry, 0, 1023, 0, 255);
+  val_rvry = constrain(val_rvry, 0, 255);
+
+#ifdef DEBUG
+  Serial.print("RVRy = ");
+  Serial.println(val_rvry);
+#endif
+
+  /* right joystick */
+  if (val_rvry == 255)
+  {
+    buf[0] = 1;
+    buf[1] = 0;
+  }
+  else if (val_rvry == 0)
+  {
+    buf[0] = 0;
+    buf[1] = 1;
+  }
+  else if (val_rvry >= 115 && val_rvry <= 135)
+  {
+    buf[0] = 0;
+    buf[1] = 0;
+  }
+
+  // panic
+  buf[5] = 0;
+
+  /* left joystick */
+  if (val_lvrx == 0)
+  {
+    buf[3] = 1;
+    buf[4] = 0;
+  }
+  else if (val_lvrx == 255)
+  {
+    buf[3] = 0;
+    buf[4] = 1;
+  }
+  else if (val_rvry >= 115 && val_rvry <= 135)
+  {
+    buf[3] = 0;
+    buf[4] = 0;
+  }
+
+  buf[5] = 0;
+
+#ifdef DEBUG
+  Serial.print("buf[left;right;honk;front;back;panic] = ");
+  for (int i = 0; i < 6; i++)
+  {
+    Serial.print(buf[i]);
+    Serial.print(";");
+  }
+  Serial.println(" ");
+#endif
+
+  rf_driver.send((uint8_t *)buf, 6);
+  rf_driver.waitPacketSent();
 }
